bfec5deb1d59f220b6e02b78f267dd2813a87bdb
Irvin Dinkins <irvdude44@gmail.com>
2023-04-23 22:38:56 -0400

    todo working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let formId = document.getElementById('create-task-form')
+  let inputId = document.getElementsByName('new-task-description')[0]
+  let ulId = document.getElementById('tasks')
+
+  function submitForm(e) {
+    e.preventDefault()
+    let inputText = inputId.value
+    console.log(inputText);
+    let newTask = document.createElement("li")
+   newTask.textContent = inputText
+    ulId.appendChild(newTask)
+    formId.reset()
+  }
+
+  formId.addEventListener("submit", submitForm)
+
 });
+
+// function buildTodo(todo) {
+//   let btn = document.createElement("button")
+//   let tasks = document.getElementById("tasks")
+//   tasks.textContent = `${todo}`
+//   tasks.appendChild(btn)
+//   console.log(todo);
+// }
